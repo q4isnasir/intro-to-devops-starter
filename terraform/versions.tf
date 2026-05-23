54e16358ed16189ecf24fb714aa022996d1ca7d7
Qais Nasir <qaisnasir123@yahoo.com>
2026-05-23 14:27:49 +0500

    Lecture 4 -   set up Terraform with local Docker provider for FruitAPI + MySQL

---
+++ terraform/versions.tf
@@ -0,0 +1,13 @@
+# Pin Terraform itself +  the providers we use.
+# Displays if the version does not match
+
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
